1173da60857e843e7398c0d4da86b03d1cc6e6d0
kapucuonur <Gumushane1983.>
2026-03-10 12:05:03 +0200

    fix: remove .history from git and fix vercel deployment

--- .history/main_20260310113412.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Portfolio from './App.js'; // Make sure this says .js
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>
-);
